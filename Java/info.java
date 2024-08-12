ccf438250da7442634e8bd3ea0f9e4eb724d861c
koda012 <lucaskojimaeda@gmail.com>
2024-08-11 21:42:12 -0300

    java

---
+++ Java/info.java
@@ -0,0 +1,27 @@
+Nome: João Silva
+Senha: Js123!@#
+Endereço: Rua das Flores, 123, São Paulo, SP
+Email: joao.silva@example.com
+Telefone: (11) 91234-5678
+
+Nome: Maria Oliveira
+Senha: Mo$987xyz
+Endereço: Avenida Central, 456, Rio de Janeiro, RJ
+Email: maria.oliveira@example.com
+Telefone: (21) 99876-5432
+
+Nome: Carlos Pereira
+Senha: Cp456*&^
+Endereço: Rua do Sol, 789, Belo Horizonte, MG
+Email: carlos.pereira@example.com
+Telefone: (31) 91234-8765
+
+Nome: Ana Souza
+Senha: As!@#789
+Endereço: Rua dos Pinheiros, 321, Curitiba, PR
+Email: ana.souza@example.com
+Telefone: (41) 98765-4321
+('João Silva', 'joao.silva@example.com', 'Js123!@#', '(11) 91234-5678', 'Rua das Flores, 123, São Paulo, SP'),
+('Maria Oliveira', 'maria.oliveira@example.com', 'Mo$987xyz', '(21) 99876-5432', 'Avenida Central, 456, Rio de Janeiro, RJ'),
+('Carlos Pereira', 'carlos.pereira@example.com', 'Cp456*&^', '(31) 91234-8765', 'Rua do Sol, 789, Belo Horizonte, MG'),
+('Ana Souza', 'ana.souza@example.com', 'As!@#789', '(41) 98765-4321', 'Rua dos Pinheiros, 321, Curitiba, PR');
