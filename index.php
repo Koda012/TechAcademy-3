a09c76f5c3ec4be884e8cea73361ffe6e45164a8
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-08-18 10:29:32 -0300

    mudanca de layout. Sidebar novo + lista de home, membros, documentacao e contato

--- index.php
+++ index.php
@@ -13,78 +13,23 @@
 </head>
 
 <body>
-    <nav id="sidebar">
-        <ul id="side_items">
-            <li class="side-item active" title="Página principal">
-                <a href="#">
-                    <i class="fa-solid fa-house"></i>
-                    <span class="item-description">
-                        Home
-                    </span>
-                </a>
-            </li>
 
-            <li class="side-item" title="Membros">
-                <a href="#">
-                    <i class="fa-solid fa-person"></i>
-                    <span class="item-description">
-                        Membros
-                    </span>
-                </a>
-            </li>
-
-            <li class="side-item" title="Documentação do Jogo">
-                <a href="#">
-                    <i class="fa-solid fa-folder"></i>
-                    <span class="item-description">
-                        Documentação Jogo
-                    </span>
-                </a>
-            </li>
-
-            <li class="side-item">
-                <a href="#">
-                    <i class="fa-solid fa-image"></i>
-                    <span class="item-description">
-                        Imagens
-                    </span>
-                </a>
-            </li>
-
-            <li class="side-item">
-                <a href="#">
-                    <i class="fa-solid fa-gear"></i>
-                    <span class="item-description">
-                        Configurações
-                    </span>
-                </a>
-            </li>
-        </ul>
-
-        <!-- <button id="open_btn">
-            <i id="open_btn_icon" class="fa-solid fa-chevron-right"></i>
-        </button> -->
-
+    <header>
+        <div class="header">
+            <button class="btn btn-login">Login</button>
+            <button class="btn btn-register">Registrar</button>
         </div>
+    </header>
 
-        <div id="logout">
-            <button id="logout_btn">
-                <i class="fa-solid fa-right-from-bracket"></i>
-                <span class="item-description">
-                    Logout
-                </span>
-            </button>
-        </div>
-    </nav>
-
-    <div class="header-login">
-        <div>
-            <img class="logo-header" src="img/logo.png" alt="Judge Now" title="Judge Now">
-            <button class="btn login-btn">Login</button>
-            <button class="btn register-btn">Registrar</button>
-        </div>
+    <div class="sidebar">
+        <h2>Judge NOW</h2>
+        <a href="#">Home</a>
+        <a href="#">Membros</a>
+        <a href="#">Documentação do jogo</a>
+        <a href="#">Contato</a>
     </div>
 
+    
     <main>
         <?php
         //imprimir o conteudo do array do GET
